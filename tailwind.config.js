7e1dfaaa5ec700d4e11da4a6fe6ddd552b175562
Marin Mirasol <mmirasol2000@gmail.com>
2024-01-05 23:59:33 -0800

    Still working on refactoring tailwindcss styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,20 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      colors: {},
-
-      // for animations
+      // for glow effect
+      dropShadow: {
+        glow: ["0 0px 20px rgba(255,255, 255, 0.35)", "0 0px 65px rgba(255, 255,255, 0.2)"],
+      },
+      // for fade in animation
       animation: {
-        fadeIn: "fadeIn 2s ease-in forwards"
+        fadeIn: "fadeIn 2s ease-in forwards",
       },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
-        }
-      }
+          "100%": { opacity: 1 },
+        },
+      },
     },
   },
   variants: {
