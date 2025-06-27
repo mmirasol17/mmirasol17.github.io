1e4fab0010946985562cec1afb3d9ed5409bf749
Marin Mirasol <mmirasol2000@gmail.com>
2025-06-26 22:01:38 -0700

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Change from "./" to "/" for custom domain
+  base: "/", // Change from "./" to "/" for custom domain
   build: {
     outDir: "dist",
     assetsDir: "assets",
