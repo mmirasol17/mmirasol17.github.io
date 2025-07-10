373b3521af09ff915c79dea4e56903f1794071a4
Marin Mirasol <mmirasol2000@gmail.com>
2025-07-09 22:05:12 -0700

    Refactor scroll logic to util func, revert hero section UX change

--- src/components/Sections/Section1/IntroFooter/index.tsx
+++ src/components/Sections/Section1/IntroFooter/index.tsx
@@ -1,18 +1,16 @@
-interface IntroFooterProps {
-  scrollToAbout: () => void;
-}
+import { handleScrollToElementById } from "../../../../utils";
 
-export function IntroFooter(props: Readonly<IntroFooterProps>) {
+export function IntroFooter() {
   return (
     <div className='mt-auto sm:mt-0 text-center items-center gap-2 mb-8 flex flex-col'>
       <button
-        onClick={props.scrollToAbout}
+        onClick={() => handleScrollToElementById("about")}
         className='text-blue-400 font-bold underline transition duration-200 hover:text-blue-300 text-lg lg:text-3xl'
       >
         Learn more about me!
       </button>
       <button
-        onClick={props.scrollToAbout}
+        onClick={() => handleScrollToElementById("about")}
         className='animate-bounce w-14 h-14 p-2 bg-gradient-to-br from-gray-400 to-gray-700 rounded-full cursor-pointer flex items-center justify-center transition duration-200 hover:scale-110 mt-4'
       >
         <img
--- src/components/Sections/Section1/IntroSection.tsx
+++ src/components/Sections/Section1/IntroSection.tsx
@@ -1,33 +1,20 @@
-import { useCallback } from "react";
 import { IntroPersonalPhoto } from "./IntroPersonalPhoto";
 import { IntroFooter } from "./IntroFooter";
 import { IntroPersonalMessage } from "./IntroPersonalMessage";
 
 export function IntroSection() {
-  const scrollToAbout = useCallback(() => {
-    const aboutSection = document.getElementById("about");
-    if (aboutSection) {
-      const navbar = document.querySelector(".navbar");
-      const offset = navbar ? navbar.clientHeight : 64;
-      window.scrollTo({
-        top: aboutSection.offsetTop - offset,
-        behavior: "smooth",
-      });
-    }
-  }, []);
-
   return (
     <header
       id='intro'
-      className='h-[100svh] px-4 bg-gradient-to-b from-blue-800 via-blue-900 to-gray-900'
+      className='min-h-[100svh] px-4 bg-gradient-to-b from-blue-800 via-blue-900 to-gray-900'
     >
       <div className='mx-auto flex flex-col justify-between sm:justify-center items-center min-h-screen'>
         <div className='transition-opacity duration-1000 opacity-100'>
           <IntroPersonalPhoto />
-          <IntroPersonalMessage scrollToAbout={scrollToAbout} />
+          <IntroPersonalMessage />
         </div>
 
-        <IntroFooter scrollToAbout={scrollToAbout} />
+        <IntroFooter />
       </div>
     </header>
   );
