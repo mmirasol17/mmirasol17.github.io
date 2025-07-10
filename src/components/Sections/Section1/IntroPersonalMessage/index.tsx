4a5d42662520a89f4f555f24ff82004fb8a42382
Marin Mirasol <mmirasol2000@gmail.com>
2025-07-09 21:58:47 -0700

    UX fixes

--- src/components/Sections/Section1/IntroPersonalMessage/index.tsx
+++ src/components/Sections/Section1/IntroPersonalMessage/index.tsx
@@ -6,8 +6,8 @@ export function IntroPersonalMessage(props: Readonly<IntroPersonalMessageProps>)
   return (
     <div className='text-center mb-10 sm:mb-16'>
       <div className='flex flex-col sm:flex-row mb-1 justify-center'>
-        <h1 className='text-3xl lg:text-5xl font-bold text-white drop-shadow-2xl'>Hello, my name is&nbsp;</h1>
-        <h1 className='text-3xl lg:text-5xl font-bold text-white drop-shadow-2xl'>
+        <h1 className='text-2xl lg:text-5xl font-bold text-white drop-shadow-2xl'>Hello, my name is&nbsp;</h1>
+        <h1 className='text-2xl lg:text-5xl font-bold text-white drop-shadow-2xl'>
           <button
             onClick={props.scrollToAbout}
             className='text-blue-400 font-bold underline transition duration-200 hover:text-blue-300'
@@ -17,7 +17,7 @@ export function IntroPersonalMessage(props: Readonly<IntroPersonalMessageProps>)
           .&nbsp;
         </h1>
       </div>
-      <h1 className='text-3xl lg:text-5xl font-bold text-white'>I'm a software engineer!</h1>
+      <h1 className='text-2xl lg:text-5xl font-bold text-white'>I'm a software engineer!</h1>
     </div>
   );
 }
--- src/components/Sections/Section1/IntroPersonalPhoto/index.tsx
+++ src/components/Sections/Section1/IntroPersonalPhoto/index.tsx
@@ -1,7 +1,7 @@
 export function IntroPersonalPhoto() {
   return (
     <div className='mt-28 mb-4 flex justify-center items-center'>
-      <div className='sm:max-w-sm w-48 h-48 sm:w-64 sm:h-64 rounded-full overflow-hidden bg-gradient-to-br from-blue-400 to-blue-600 flex items-center justify-center'>
+      <div className='sm:max-w-sm w-40 h-40 sm:w-64 sm:h-64 rounded-full overflow-hidden bg-gradient-to-br from-blue-400 to-blue-600 flex items-center justify-center'>
         <div className='text-6xl'>
           <img
             alt='Marin Mirasol'
