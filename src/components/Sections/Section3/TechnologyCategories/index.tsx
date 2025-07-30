8b8e7a4a0e4126de3187f02c0ae41f33be17a6d0
Marin Mirasol <mmirasol2000@gmail.com>
2025-07-29 21:27:06 -0700

    Fix projects expand/collapse bug

--- src/components/Sections/Section3/TechnologyCategories/index.tsx
+++ src/components/Sections/Section3/TechnologyCategories/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useTechnologies } from "../../../../hooks/useTechnologies";
 import { Minus, Plus } from "lucide-react";
 import { TechnologyCategory } from "./TechnologyCategory";
@@ -27,6 +27,16 @@ export function TechnologyCategories() {
     setCollapsedSections({});
   }, [technologyCategories]);
 
+  // Calculate how many sections are actually collapsed
+  const collapsedCount = useMemo(() => {
+    return technologyCategories.filter((category) => collapsedSections[category.category] === true).length;
+  }, [collapsedSections, technologyCategories]);
+
+  // Calculate how many sections are expanded
+  const expandedCount = useMemo(() => {
+    return technologyCategories.length - collapsedCount;
+  }, [collapsedCount, technologyCategories.length]);
+
   return (
     <div className='container mx-auto max-w-7xl'>
       <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8 gap-4'>
@@ -39,7 +49,7 @@ export function TechnologyCategories() {
             <button
               onClick={expandAllSections}
               // Disable button if all sections are expanded
-              disabled={Object.keys(collapsedSections).length === 0}
+              disabled={expandedCount === technologyCategories.length}
               className='bg-blue-500 hover:bg-blue-600 text-white md:px-3 md:py-1.5 px-8 py-3 rounded text-sm transition flex items-center gap-1 disabled:opacity-50 disabled:cursor-not-allowed'
             >
               <Plus className='w-3 h-3' />
@@ -48,7 +58,7 @@ export function TechnologyCategories() {
             <button
               onClick={collapseAllSections}
               // Disable button if all sections are collapsed
-              disabled={Object.keys(collapsedSections).length === technologyCategories.length}
+              disabled={collapsedCount === technologyCategories.length}
               className='bg-gray-600 hover:bg-gray-500 text-white md:px-3 md:py-1.5 px-8 py-3 rounded text-sm transition flex items-center gap-1 disabled:opacity-50 disabled:cursor-not-allowed'
             >
               <Minus className='w-3 h-3' />
