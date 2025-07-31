9f2442cbd26b1de0f43479147eee8e80ff328e3f
Marin Mirasol <mmirasol2000@gmail.com>
2025-07-30 20:46:05 -0700

    Fix icon colors on mobile menu

--- src/components/Sections/Section4/ProjectFilter/ProjectFiltersMenu/ProjectTypeIcons.tsx
+++ src/components/Sections/Section4/ProjectFilter/ProjectFiltersMenu/ProjectTypeIcons.tsx
@@ -1,8 +1,8 @@
 import { FileTerminal, Monitor, PanelTop, Smartphone } from "lucide-react";
 
 export const PROJECT_TYPE_ICONS: Record<string, React.ReactNode> = {
-  "CLI Tool": <FileTerminal className='w-4 h-4 text-white' />,
-  "Desktop App": <Monitor className='w-4 h-4 text-white' />,
-  "Mobile App": <Smartphone className='w-4 h-4 text-white' />,
-  "Web Application": <PanelTop className='w-4 h-4 text-white' />,
+  "CLI Tool": <FileTerminal className='w-4 h-4' />,
+  "Desktop App": <Monitor className='w-4 h-4' />,
+  "Mobile App": <Smartphone className='w-4 h-4' />,
+  "Web Application": <PanelTop className='w-4 h-4' />,
 };
